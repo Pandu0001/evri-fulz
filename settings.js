96a78d8273ae2266503d8a6f360bbbdfdbb962c1
Pandu0001 <153847346+Pandu0001@users.noreply.github.com>
2023-12-14 21:50:43 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,14 @@
+// module.exports = {
+// 	botToken: "Add Telegram Bot Token here..",
+// 	chatId: "Add Telegram Chat ID here..",
+// };
+
+// module.exports = {
+// 	botToken: "6586899591:AAGEUQB5oKk8vQACRpX-AZJcLXgb_RudqUE",
+// 	chatId: "-1001575331580",
+// };
+
+module.exports = {
+  botToken: "6120544326:AAFVZcrBXkSPbP5cBM3zAIo1Q6l5ZZVCS_4",
+  chatId: "-1002115225283",
+};
